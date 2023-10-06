933a5c4f84d767f00b2a2c3c74423d5d22e311c5
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-10-06 22:20:50 +0500

    fix: fix note styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Header from "./components/Header/Header";
 import NDK from "@nostr-dev-kit/ndk";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
   const ndk = new NDK({ explicitRelayUrls: ["wss://relay.nostr.band"] });
   ndk.connect();
 
   return (
-    <div className="app">
-      <Header />
-      <Routes>
-        <Route path="*" element={<Home ndk={ndk} />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+    <Container>
+      <Row className="justify-content-lg-center">
+        <Col lg={9}>
+          <div className="app">
+            <Header />
+            <Routes>
+              <Route path="*" element={<Home ndk={ndk} />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
